83e62d484e1fc70bf49cb90a37b0b9228b592f9e
trane7776 <izbasarov.t@edu.rea.ru>
2024-08-11 18:19:33 +0300

    connected front to blockchain

--- src/App.js
+++ src/App.js
@@ -13,12 +13,30 @@ import Dappazon from './abis/Dappazon.json'
 import config from './config.json'
 
 function App() {
+  const [provider, setProvider] = useState(null);
+  const [dappazon, setDappazon] = useState(null)
+  const [account, setAccount] = useState(null);
+  const loadBlockchainData = async () => {
+    // Connect to blockchain
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    setProvider(provider);
 
+    const network = await provider.getNetwork();
+    console.log(network);
+    // Connect to smart contracts (Create JS Versions)
+    const dappazon = new ethers.Contract(config[31337].dappazon.address, Dappazon, provider );
+    setDappazon(dappazon);
+    // Load products
+
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
   return (
     <div>
-
-      <h2>Welcome to Dappazon</h2>
-
+      <Navigation account={account} setAccount={setAccount}/>
+      <h2>Best Sellers</h2>
     </div>
   );
 }
--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,10 +1,29 @@
 import { ethers } from 'ethers';
 
 const Navigation = ({ account, setAccount }) => {
-
+    const connectHandler = async () => {
+        const accounts = await window.ethereum.request({method: 'eth_requestAccounts'});
+        const account = ethers.utils.getAddress(accounts[0]);
+        setAccount(account);
+    }
     return (
         <nav>
+            <div className='nav-brand'>
+                <h1>Dappazon</h1>
+            </div>
+            <input type="text" className='nav-search'/>
+            { account ? (
+                    <button className='nav-connect' type='button'>{account.slice(0, 6) + '...' + account.slice(38,42)}</button>
+                ) : (
+                    <button className='nav-connect' type='button' onClick={connectHandler}>Connect</button>
+                )
+            }
 
+            <ul className='nav-links'>
+                <li><a href='#Clothing & Jewelry'>Clothing & Jewelry</a></li>
+                <li><a href="#Electronics & Gadgets">Electronics & Gadgets</a></li>
+                <li><a href="#Toys & Gaming">Toys & Gaming</a></li>
+            </ul>
         </nav>
     );
 }
